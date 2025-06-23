fc4b86c0bdc6293efcae43d4a10fb839b3ce06d1
Tsvetanmarkov <145834423+Markowcvetan@users.noreply.github.com>
2025-06-23 12:49:47 +0300

    add changes to login form

--- frontend/interview-preparation-app/src/components/Modal.jsx
+++ frontend/interview-preparation-app/src/components/Modal.jsx
@@ -2,23 +2,24 @@ import React from "react";
 
 function Modal({ children, isOpen, onClose, title, hideHeader }) {
   return (
-    <div className="flex inset-0 z-50 flex justify-center items-center w-full h-full">
+    <div className="fixed inset-0 z-50 flex justify-center w-full h-full items-center bg-black/40">
       {/* Modal contetnt */}
       <div
-        className={`relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden`}
+        className={`relative flesx flex-col bg-white shadow-lg rounded-lg overflow-hidden`}
       >
         {/* Modal Header */}
+
         {!hideHeader && (
-          <div className="flex items-center justify-between p-y border-b border-gray-200">
-            <h3 className="md:text-lg font-medium text-gray-900">{title}</h3>
+          <div className="">
+            <h3 className="">{title}</h3>
           </div>
         )}
 
-        <button type="button" className="text-gray-400 bg-transparent hover:bg-orange-100 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex jsutify-center items-center absolute top-3.5 righ-3.5 cursor-pointer" onClick={onClose}>
+        <button type="button" className="" onClick={onClose}>
           <svg
             className=""
             aria-hidden="true"
-            xmlns="htttp://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 14 14"
           >
@@ -27,14 +28,12 @@ function Modal({ children, isOpen, onClose, title, hideHeader }) {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M1 1l6 6m0 6M7 7l6-6M7 7l-6 6"
+              d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7l-6 6"
             />
           </svg>
         </button>
-
         {/* Modal Body (Scrollable) */}
-
-        <div className="flex-1 overflow-y-auto cursor-scrollbar">{children}</div>
+        <div className="">{children}</div>
       </div>
     </div>
   );
--- frontend/interview-preparation-app/src/pages/Auth/Login.jsx
+++ frontend/interview-preparation-app/src/pages/Auth/Login.jsx
@@ -11,10 +11,18 @@ function Login({ setCurrentPage }) {
   //  Handle Login Form Submit
   const handleLogin = async (e) => {
     e.preventDefault();
-    
-  }
+  };
 
-  return <div></div>;
+  return (
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter your details to log in{" "}
+      </p>
+
+      <form onSubmit={handleLogin}></form>
+    </div>
+  );
 }
 
 export default Login;
