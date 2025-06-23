98b9b1d998368df6f1160de7ecbfd8c73b647222
Tsvetanmarkov <145834423+Markowcvetan@users.noreply.github.com>
2025-06-23 12:13:16 +0300

    finish off modal + some changes to login Page

--- frontend/interview-preparation-app/src/pages/Auth/Login.jsx
+++ frontend/interview-preparation-app/src/pages/Auth/Login.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
-  return (
-    <div>
-      <p>Login</p>
-    </div>
-  );
+function Login({ setCurrentPage }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  //  Handle Login Form Submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+  }
+
+  return <div></div>;
 }
 
 export default Login;
