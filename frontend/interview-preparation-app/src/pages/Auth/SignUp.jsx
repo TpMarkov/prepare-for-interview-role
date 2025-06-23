753743258afa92d2d27b7cb4b9a13eee90b4d224
Tsvetanmarkov <145834423+Markowcvetan@users.noreply.github.com>
2025-06-23 15:07:41 +0300

    add sign up form + styles

--- frontend/interview-preparation-app/src/pages/Auth/SignUp.jsx
+++ frontend/interview-preparation-app/src/pages/Auth/SignUp.jsx
@@ -1,4 +1,6 @@
-import { useState, useNavigate } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Input from "../../components/Inputs/Input";
 
 function SignUp({ setCurrentPage }) {
@@ -18,16 +20,51 @@ function SignUp({ setCurrentPage }) {
   };
 
   return (
-    <div className="">
-      <h3 className="">Create an Account</h3>
-      <p className="">Join us today by entering your details below.</p>
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-ls font-semibold text-black">Create an Account</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Join us today by entering your details below.
+      </p>
 
-      <form className="" onSubmit={handleSignUp}>
-        <div className="">
-          <Input 
-          value={fullName}
-          onChange={({target}) => setFullName(target.value) }
+      <form onSubmit={handleSignUp}>
+        <div className="grid grid-cols-1 md:grid-cols-1 gap-2">
+          <Input
+            value={fullName}
+            label="John"
+            placeholder="John"
+            onChange={({ target }) => setFullName(target.value)}
+            type="text"
+          />
 
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            labe="Email Address"
+            placeholder="john@emample.com"
+            type="text"
+          />
+
+          <Input
+            value={password}
+            type="password"
+            label="Password"
+            placeholder="Min 8 characters"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button className="btn-primary" type="submit">
+          SIGN UP
+        </button>
+        <p className="text-[13px] text-slate-800 mt-3">
+          Already have an account?{" "}
+          <button
+            className="text-primary font-medium underline cursor-pointer"
+            onClick={() => setCurrentPage("login")}
+          >
+            Login
+          </button>
+        </p>
       </form>
     </div>
   );
