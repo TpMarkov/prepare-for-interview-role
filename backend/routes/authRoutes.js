f4936f83e878b0c2ca2903e9193e3cf62a47c383
Tsvetanmarkov <145834423+Markowcvetan@users.noreply.github.com>
2025-06-25 10:57:44 +0300

    add some comments to help potential employers better understand the code

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -11,9 +11,13 @@ const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 //  Auth Routes
-
+//  Register user POST @ /api/auth/register
 router.post("/register", registerUser); //  Register User
+
+//  Login user POST @ /api/auth/login
 router.post("/login", loginUser); //    Login User
+
+//  Get user profile details GET @ /api/auth/profile
 router.get("/profile", protect, getUserProfile); // Get User Details
 
 module.exports = router;
