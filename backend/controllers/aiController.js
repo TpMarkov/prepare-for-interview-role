b21e1d2f2c7eee80963d9a9f0d0c6829281b1e75
Tsvetanmarkov <145834423+Markowcvetan@users.noreply.github.com>
2025-06-30 08:42:59 +0300

    add some changes to ai prompt to shorten the explanation to questions

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -33,6 +33,8 @@ const generateInterviewQuestions = async (req, res) => {
     const response = await result.response;
     const rawText = await response.text();
 
+    // Clean remove the ```json and ``` from beggining and end
+
     const cleanedText = rawText
       .replace(/^```json\s*/, "")
       .replace(/```$/, "")
@@ -56,6 +58,44 @@ const generateInterviewQuestions = async (req, res) => {
 //  @desc Generate explains a interview question
 //  @route POST /api/ai/generate-explanation
 //  @access Private
-const generateConceptExplanation = async (req, res) => {};
+const generateConceptExplanation = async (req, res) => {
+  try {
+    const { question } = req.body;
+    if (!question) {
+      return res
+        .status(400)
+        .json({ message: "Missing required fields ('question')." });
+    }
+
+    const prompt = conceptExplainPrompt(question);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const rawText = await response.text();
+
+    // Remove any markdown wrappers
+    const cleanedText = rawText
+      .replace(/^```json\s*/, "")
+      .replace(/```$/, "")
+      .trim();
+
+    // Extract JSON safely using regex
+    const jsonMatch = cleanedText.match(/{[\s\S]*}/);
+    if (!jsonMatch) {
+      throw new Error("No valid JSON object found in AI response.");
+    }
+
+    const data = JSON.parse(jsonMatch[0]);
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error generating concept explanation:", error);
+    res.status(500).json({
+      message: "Failed to generate Questions.",
+      error: error.message,
+    });
+  }
+};
 
 module.exports = { generateInterviewQuestions, generateConceptExplanation };
